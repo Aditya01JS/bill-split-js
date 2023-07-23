ea5c341859d6b8cb07141346fab7106dbc11f549
Aditya01JS <adityajanardan@techis.io>
2023-07-24 05:06:22 +0530

    js functions partially added

---
+++ bill.js
@@ -0,0 +1,40 @@
+
+
+let amountInput = document.querySelector("#amount");
+let friendsInput = document.querySelector("#friends");
+// let result = ""
+let Txt = document.querySelector(".ttmm");
+// const news = document.createElement("")
+// news.setAttribute("class", "ttmmResult")
+// Txt.appendChild(news)
+// let resultTxt = document.querySelector(".ttmmResult")
+function calc(){
+    
+    let calculate = eval(amountInput.value/friendsInput.value)
+    console.log(calculate)
+  
+    Txt.textContent = calculate;
+
+
+}
+
+
+let divElement = document.getElementById("myDiv");
+
+divElement.addEventListener("click", function() {
+
+  // Remove the "placeholder" class when the div is clicked
+  divElement.classList.remove("placeholder");
+  // Clear the placeholder text
+  if (divElement.textContent === "Click here to enter text...") {
+    divElement.textContent = "";}
+ 
+});
+
+// divElement.addEventListener("blur", function() {
+//   // Add the "placeholder" class and restore placeholder text if the div is empty
+//   if (!divElement.textContent.trim()) {
+//     divElement.textContent = "Click here to enter text...";
+//     divElement.classList.add("placeholder");
+//   }
+// });
